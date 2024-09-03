35697599cce7d472f560cf6ea59ce68133472d40
Matt Dow <matt.dow.vo@gmail.com>
2024-09-03 17:01:35 -0500

    Add very basic layout styling and initial content.

--- matt-dow-resume-builder/src/App.vue
+++ matt-dow-resume-builder/src/App.vue
@@ -1,17 +1,75 @@
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main class="container">
+    <div id="resume" class="d-flex">
+      <div class="left-col">
+        <h4 class="section-headline">
+          About Me
+        </h4>
+        <div>
+          Software development professional with expertise in mobile and web applications, blending technical acumen with strong problem-solving and collaboration skills. Proven ability to deliver impactful solutions by leveraging experience in both tech and customer-facing roles from a previous career in commercial property management.
+        </div>
+        <h4 class="section-headline">
+          Contact
+        </h4>
+        <ul>
+          <li>1-651-587-8454</li>
+          <li>matt.dow.vo@gmail.com</li>
+          <li>514 Portland Avenue, Saint Paul, MN, 55102</li>
+        </ul>
+        <h4 class="section-headline">
+          Skills
+        </h4>
+        <ul>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>React Native</li>
+          <li>Vue</li>
+          <li>Node.js</li>
+          <li>Google Cloud Platform</li>
+          <li>SQL</li>
+        </ul>
+        <h4 class="section-headline">
+          Certifications
+        </h4>
+        <ul>
+          <li>Data Analytics (General Assembly Online)</li>
+          <li>Full-Stack Software Engineering (Prime Digital Academy)</li>
+        </ul>
+      </div>
+      <div class="right-col">
+        <h4 class="personal-name">
+          Matt Dow
+        </h4>
+        <h4 class="personal-title">
+          Software Developer
+        </h4>
+        <h4 class="section-headline">
+          Experience
+        </h4>
+        <h4 class="section-headline">
+          Education
+        </h4>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-     
-  </main> -->
-  resume builder to come soon
+  </main>
 </template>
+
+<style scoped>
+  #resume {
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+  /* DIN A4 standard paper size. commonly used for resumes
+  For North America letter size use width: 8.5in; height: 11in; */
+  height: 297mm;
+  width: 210mm;
+  }
+
+  .left-col {
+    width: 30%;
+  }
+
+  .right-col {
+    width: 70%;
+  }
+</style>
 
