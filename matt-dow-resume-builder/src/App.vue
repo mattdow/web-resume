58a6c7afcc7bfde985de5753125c58cb68f4231a
Matt Dow <matt.dow.vo@gmail.com>
2024-09-17 09:36:41 -0500

    Complete resume content

--- matt-dow-resume-builder/src/App.vue
+++ matt-dow-resume-builder/src/App.vue
@@ -47,9 +47,69 @@
         <h4 class="section-headline">
           Experience
         </h4>
+        <div>Software Developer</div>
+        <div>BetterYou, Saint Paul, MN, 03/2022 - 07/2024</div>
+        <ul>
+          <li>
+            Rebuilt the app's sleep system in 2024, employing a new machine learning model and a new user-friendly interface to help users establish consistent bedtime routines.
+          </li>
+          <li>
+            During the company's 2022 app and database overhaul, completely updated critical systems to modern development standards.
+          </li>
+          <li>
+            Designed and implemented seamless system integrations for aggregating health and wellness data from a variety of sources, including Apple Health, Google Fit, and wearable devices.
+          </li>
+           <li>
+            Led the Android app through a critical and time-sensitive Google Trust and Safety security assessment in May 2023, including the successful completion of a CASA security scan and subsequent thorough app review.
+          </li>
+          <li>
+            Key Technologies Utilized: React Native, React Query, JavaScript, Node.js, Google Cloud Platform, Firebase, Swift, Java, Jest, and JUnit.
+          </li>
+        </ul>
+        <div>Full Stack Software Engineering Student</div>
+        <div>Prime Digital Academy, Minneapolis, MN, 08/2021-01/2022</div>
+        <ul>
+          <li>
+            Coordinated and collaborated with developers, project managers, and UX designers for the client to build a proof-of-concept software marketplace prototype in a two-week sprint.
+          </li>
+          <li>
+            Following Prime graduation, assisted the client's engineering team to convert the codebase to TypeScript.
+          </li>
+          <li>
+            As a solo capstone project, created a disc golf score and note tracker.
+          </li>
+          <li>
+            Technologies used: JavaScript, TypeScript, Node.js, Express, React.js, Redux, Saga, Chart.js, PostgreSQL, and Material-UI.
+          </li>
+        </ul>
+        <div>Real Estate Manager</div>
+        <div>Ryan Companies, Minneapolis, MN, 09/2011-05/2020</div>
+        <ul>
+          <li>
+            Managed a 650,000 square foot class-A office building with two colleagues.
+          </li>
+          <li>
+            Created a plan to sustain Ryan's culture as part of Ryan's 2015-16 Emerging Leaders Group.
+          </li>
+          <li>
+            Raised over $300,000 during dozens of events as co-ambassador of Ryan's 2016 United Way Week.
+          </li>
+          <li>
+            Facilitated a full lease turnover and four major tenant improvement projects over two years as lead manager for a downtown office building.
+          </li>
+          <li>
+            Improved janitorial service, security, window cleaning, and elevator maintenance by crafting and enforcing contract language and regularly meeting with vendors.
+          </li>      
+        </ul>
         <h4 class="section-headline">
           Education
         </h4>
+        <div>Certificate in Full Stack Software Engineering</div>
+        <div>Prime Digital Academy, 08/2021-01/2022</div>
+        <div>Certificate in Data Analytics</div>
+        <div>General Assembly, 06/2019-08/2019</div>
+        <div>Courses taken toward a Chemical Engineering Degree</div>
+        <div>University of Wisconsin-Madison, 1997-2001</div>        
       </div>
     </div>
   </main>
