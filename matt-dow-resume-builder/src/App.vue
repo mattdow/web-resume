cb2671abb9179b911491cd567f108e1ed4507c4a
Matt Dow <matt.dow.vo@gmail.com>
2024-09-17 11:04:35 -0500

    Add headshot, move template content into state data object. Adjust styling.

--- matt-dow-resume-builder/src/App.vue
+++ matt-dow-resume-builder/src/App.vue
@@ -4,40 +4,35 @@
     <div id="resume" class="d-flex">
       <div class="left-col">
         <div class="resume-section">
+          <img v-bind:src="imageUrl" class="profile-pic" alt="profile picture">
           <div class="section-headline">
-            About Me
+            {{ headlines[0] }}
           </div>
           <div>
-            Software development professional with expertise in mobile and web applications, blending technical acumen with strong problem-solving and collaboration skills. Proven ability to deliver impactful solutions by leveraging experience in both tech and customer-facing roles from a previous career in commercial property management.
+            {{ introText }}
           </div>
         </div>
         <div class="resume-section">
           <div class="section-headline">
-            Contact
+            {{ headlines[1] }}
           </div>
           <ul>
-            <li>651-587-8454</li>
-            <li>matt.dow.vo@gmail.com</li>
-            <li>514 Portland Avenue, Saint Paul, MN, 55102</li>
+            <li>{{ contactInfo.phone }}</li>
+            <li>{{ contactInfo.email }}</li>
+            <li>{{ contactInfo.address }}</li>
           </ul>
         </div>
         <div class="resume-section">
           <div class="section-headline">
-            Skills
+            {{ headlines[2] }}
           </div>
           <ul>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>React Native</li>
-            <li>Vue</li>
-            <li>Node.js</li>
-            <li>Google Cloud Platform</li>
-            <li>SQL</li>
+            <li v-for="(skill, index) in skills" :key="index">{{ skill }}</li>
           </ul>
         </div>
         <div class="resume-section">
           <div class="section-headline">
-            Certifications
+            {{ headlines[3] }}
           </div>
           <ul>
             <li>Data Analytics (General Assembly Online)</li>
@@ -47,77 +42,104 @@
       </div>
       <div class="right-col">
         <h4 class="personal-name">
-          Matt Dow
+          {{ name }}
         </h4>
         <h4 class="personal-title">
-          Software Developer | Technical Support Engineer
+          {{ title }}
         </h4>
         <h4 class="section-headline">
           Experience
         </h4>
-        <div>Software Developer</div>
-        <div>BetterYou, Saint Paul, MN, 03/2022 - 07/2024</div>
-        <ul>
-          <li>
-            Rebuilt the app's sleep system in 2024, employing a new machine learning model and a new user-friendly interface to help users establish consistent bedtime routines.
-          </li>
-          <li>
-            During the company's 2022 app and database overhaul, completely updated critical systems to modern development standards.
-          </li>
-          <li>
-            Designed and implemented seamless system integrations for aggregating health and wellness data from a variety of sources, including Apple Health, Google Fit, and wearable devices.
-          </li>
-           <li>
-            Led the Android app through a critical and time-sensitive Google Trust and Safety security assessment in May 2023, including the successful completion of a CASA security scan and subsequent thorough app review.
-          </li>
-          <li>
-            Key Technologies Utilized: React Native, React Query, JavaScript, Node.js, Google Cloud Platform, Firebase, Swift, Java, Jest, and JUnit.
-          </li>
-        </ul>
-        <div>Full Stack Software Engineering Student</div>
-        <div>Prime Digital Academy, Minneapolis, MN, 08/2021-01/2022</div>
-        <ul>
-          <li>
-            Coordinated and collaborated with developers, project managers, and UX designers for the client to build a proof-of-concept software marketplace prototype in a two-week sprint.
-          </li>
-          <li>
-            Following Prime graduation, assisted the client's engineering team to convert the codebase to TypeScript.
-          </li>
-          <li>
-            As a solo capstone project, created a disc golf score and note tracker.
-          </li>
-          <li>
-            Technologies used: JavaScript, TypeScript, Node.js, Express, React.js, Redux, Saga, Chart.js, PostgreSQL, and Material-UI.
-          </li>
-        </ul>
-        <div>Real Estate Manager</div>
-        <div>Ryan Companies, Minneapolis, MN, 09/2011-05/2020</div>
-        <ul>
-          <li>
-            Managed a 650,000 square foot class-A office building with two colleagues.
-          </li>
-          <li>
-            Created a plan to sustain Ryan's culture as part of Ryan's 2015-16 Emerging Leaders Group.
-          </li>
-          <li>
-            Raised over $300,000 during dozens of events as co-ambassador of Ryan's 2016 United Way Week.
-          </li>
-          <li>
-            Facilitated a full lease turnover and four major tenant improvement projects over two years as lead manager for a downtown office building.
-          </li>
-          <li>
-            Improved janitorial service, security, window cleaning, and elevator maintenance by crafting and enforcing contract language and regularly meeting with vendors.
-          </li>      
-        </ul>
+        <div v-for="(item, index) in experience" :key="index" class="inner-section">
+          <div>{{ item.title }}</div>
+          <div class="d-flex justify-content-between">
+            <div>{{ item.company }}, {{ item.location }}</div>
+            <div>{{ item.date }}</div>
+          </div>
+          <ul>
+            <li v-for="(desc, innerIndex) in item.description" :key="innerIndex">{{ desc }}</li>
+          </ul>
+        </div>
+        
         <h4 class="section-headline">
           Education
         </h4>
-        <div>Courses taken toward a Chemical Engineering Degree</div>
-        <div>University of Wisconsin-Madison, 1997-2001</div>        
+        <div>{{ education.title }}</div>
+        <div class="d-flex justify-content-between">
+          <div>{{ education.university }}</div>
+          <div>{{ education.date }}</div>
+        </div>  
       </div>
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Matt Dow",
+      title: "Software Developer | Technical Support Engineer",
+      introText: "Software development professional with expertise in mobile and web applications, blending technical acumen with strong problem-solving and collaboration skills. Proven ability to deliver impactful solutions by leveraging experience in both tech and customer-facing roles from a previous career in commercial property management.",
+      imageUrl: "/matt_dow_headshot.jpg",
+      headlines: ["About me", "Contact", "Skills", "Certifications", "Experience", "Education"],
+      contactInfo: {
+        phone: "651-587-8454",
+        email: "matt.dow.vo@gmail.com",
+        address: "514 Portland Avenue, Saint Paul, MN 55102"
+      },
+      skills: ["JavaScript", "React", "React Native", "Vue", "Node.js", "Google Cloud Platform", "SQL"],
+      experience: [
+        {
+          title: "Software Developer",
+          company: "BetterYou",
+          location: "Saint Paul, MN",
+          date: "03/2022 - 07/2024",
+          description: [
+            "Rebuilt the app's sleep system in 2024, employing a new machine learning model and a new user-friendly interface to help users establish consistent bedtime routines.",
+            "During the company's 2022 app and database overhaul, completely updated critical systems to modern development standards.",
+            "Designed and implemented seamless system integrations for aggregating health and wellness data from a variety of sources, including Apple Health, Google Fit, and wearable devices.",
+            "Led the Android app through a critical and time-sensitive Google Trust and Safety security assessment in May 2023, including the successful completion of a CASA security scan and subsequent thorough app review.",
+            "Key Technologies Utilized: React Native, React Query, JavaScript, Node.js, Google Cloud Platform, Firebase, Swift, Java, Jest, and JUnit.",
+          ]
+        },
+        {
+          title: "Full Stack Software Engineering Student",
+          company: "Prime Digital Academy",
+          location: "Minneapolis, MN",
+          date: "08/2021 - 01/2022",
+          description: [
+            "Coordinated and collaborated with developers, project managers, and UX designers for the client to build a proof-of-concept software marketplace prototype in a two-week sprint.",
+            "Following Prime graduation, assisted the client's engineering team to convert the codebase to TypeScript.",
+            "As a solo capstone project, created a disc golf score and note tracker.",
+            "Technologies used: JavaScript, TypeScript, Node.js, Express, React.js, Redux, Saga, Chart.js, PostgreSQL, and Material-UI.",
+          ]
+        },
+        {
+          title: "Real Estate Manager",
+          company: "Ryan Companies",
+          location: "Minneapolis, MN",
+          date: "2011 - 2020",
+          description: [
+            "Managed a 650,000 square foot class-A office building with two colleagues.",
+            "Created a plan to sustain Ryan's culture as part of Ryan's 2015-16 Emerging Leaders Group.",
+            "Raised over $300,000 during dozens of events as co-ambassador of Ryan's 2016 United Way Week.",
+            "Facilitated a full lease turnover and four major tenant improvement projects over two years as lead manager for a downtown office building.",
+            "Improved janitorial service, security, window cleaning, and elevator maintenance by crafting and enforcing contract language and regularly meeting with vendors.",
+          ]
+        }
+      ],
+      education: 
+        {
+          title: "Courses taken toward a Chemical Engineering Degree",
+          university: "University of Wisconsin-Madison",
+          date: "1997 - 2001",
+        },
+      
+    }
+  }
+}
+</script>
 
 <style scoped>
   #resume {
