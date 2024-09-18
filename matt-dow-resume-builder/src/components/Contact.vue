f6f3f56d5a3cf98d481949a2fb722b69679fd290
Matt Dow <matt.dow.vo@gmail.com>
2024-09-18 16:59:02 -0500

    Create Contact component

---
+++ matt-dow-resume-builder/src/components/Contact.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li 
+      contenteditable="true" 
+      @input="TODO"
+    >
+      {{ contactInfo.phone }}
+    </li>
+    <li 
+      contenteditable="true" 
+      @input="updateNestedProperty($event, 'contactInfo', 'email')"
+    >
+      {{ contactInfo.email }}
+    </li>
+    <li 
+      contenteditable="true" 
+      @input="updateNestedProperty($event, 'contactInfo', 'address')"
+    >
+      {{ contactInfo.address }}
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: ["contact"]
+  }
+</script>
